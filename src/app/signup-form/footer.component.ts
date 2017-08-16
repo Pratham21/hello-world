4d88fed9fc982cdf1f4cab5e1521ae730e34b0fc
Pratham Bharadwaj <prathambharadwaj21@gmail.com>
2017-08-16 17:11:01 -0400

    Changed Sign up
    Stable commit: Used angular to design UI for form

--- src/app/signup-form/footer.component.ts
+++ src/app/signup-form/footer.component.ts
@@ -4,7 +4,14 @@ import { Component, OnInit } from '@angular/core';
   selector: 'footer',
   template: `
         <h1 class="footer">Contact us: m2k17@miraclesoft.com</h1>
-        
+        <div class="container">
+    <a class="logo-font" routerLink="/">conduit</a>
+    <span class="attribution">
+      &copy; {{ today | date: 'yyyy' }}.
+      An interactive learning project from <a href="https://thinkster.io">Thinkster</a>.
+      Code licensed under MIT.
+    </span>
+  </div> 
   `,
   styleUrls: []
 })
